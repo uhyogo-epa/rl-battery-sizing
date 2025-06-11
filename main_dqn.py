2f2fe72ed1b11e9590336f49fc8f897fe6a2708e
mantani0220 <167833915+mantani0220@users.noreply.github.com>
2025-06-11 11:50:05 +0900

    Update main_dqn.py

--- main_dqn.py
+++ main_dqn.py
@@ -4,7 +4,7 @@ import random
 from datetime import datetime
 from agent.dqn import DQN
 from multiprocessing import Process
-
+import ray
 # Environment
 # from rl_env.energy import RenewableEnergyEnv
 from rl_env.energy_lstm import EnergyLSTMEnv
@@ -12,15 +12,17 @@ from rl_env.energy_lstm import EnergyLSTMEnv
 ##########################################################
 # Main     
 ##########################################################
-# def run_training(seed,  battery_times):
-if __name__ == "__main__":
-    random.seed(1)
-    np.random.seed(1)
+ray.init(ignore_reinit_error = True)
+
+@ray.remote
+def run_training(seed,  battery_times, learnig_rate):
+# if __name__ == "__main__":
+    random.seed(seed)
+    np.random.seed(seed)
     # Environment
     # env               = RenewableEnergyEnv(mode = 'discrete')
     mode = 'discrete'
-    battery_times = 1.0
-    env               = EnergyLSTMEnv(mode ,battery_times)
+    env               = EnergyLSTMEnv(mode ,battery_times,learning_rate)
     state_space       = env.observation_space
     action_space      = env.action_space
 
@@ -30,7 +32,7 @@ if __name__ == "__main__":
     agent = DQN(state_space, action_space,
                           ## Learning rate
                           CRITIC_LEARN_RATE   = 1e-4,  
-                          DISCOUNT            = 0.9,                             
+                          DISCOUNT            = 0.99,                             
                           ## DQN options 
                           REPLAY_MEMORY_SIZE  = 5000, 
                           REPLAY_MEMORY_MIN   = 100,
@@ -45,8 +47,8 @@ if __name__ == "__main__":
     # training
     #########################################
     
-    LOG_DIR = f'dqn_logs/test_run_{datetime.now().strftime("%m%d_%H%M")}'
-    # LOG_DIR = f'dqn_lstm_logs/test_run_{datetime.now().strftime("%m%d_%H%M")}_seed_{seed}_battery_{round(battery_times,2)}'
+    # LOG_DIR = f'dqn_logs/test_run_{datetime.now().strftime("%m%d_%H%M")}_seed_{seed}'
+    LOG_DIR = f'dqn_lstm_logs_com/battery_{round(battery_times,2)}/test_run_seed_{seed}_battery_{round(battery_times,2)}'
 
     agent.train(env, 
                 EPISODES      = 1000, 
@@ -54,22 +56,23 @@ if __name__ == "__main__":
                 LOG_DIR       = LOG_DIR,
                 SAVE_AGENTS   = True, 
                 SAVE_FREQ     = 500,
-                seed = 1,
+                seed = seed,
+                battery_times = battery_times
                 )
 
-# if __name__ == "__main__":
-#     seeds = [1,2,3,4,5]  
-#     processes = []
-#     battery_times  = 0.25
-#     battery_times_max = 1.0
-#     while  battery_times  <=  battery_times_max:
-#         processes = []
-#         for seed in seeds:
-#             p = Process(target=run_training, args=(seed, battery_times ))
-#             p.start()
-#             processes.append(p)
+
+if __name__ == "__main__":
+    seeds = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    battery_times =0.4
+    battery_times_max = 1.0
+    learning_rates = [1e-7]
+    while battery_times <= battery_times_max:
+        tasks = []
+        for learning_rate in learning_rates:
+            for seed in seeds:
+                task = run_training.remote(seed,battery_times,learning_rate)
+                tasks.append(task)
+            
+        ray.get(tasks)
         
-#         for p in processes:
-#             p.join()
-        
-#         battery_times += 0.25
+        battery_times += 0.2
